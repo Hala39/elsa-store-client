c74abb05a6bca4baa5dc53acc2261d3bdeac2e01
F&H <laphala.11@gmail.com>
2021-09-29 02:27:06 +0300

    router

--- src/app/Guards/order.guard.ts
+++ src/app/Guards/order.guard.ts
@@ -1,6 +1,6 @@
 import { BasketService } from './../Services/basket.service';
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -8,12 +8,13 @@ import { Observable } from 'rxjs';
 })
 export class OrderGuard implements CanActivate {
 
-  constructor(private basketService: BasketService) {}
+  constructor(private basketService: BasketService, private router: Router) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     if (this.basketService.orderItemsSource.value === null) {
+      this.router.navigateByUrl('/');
       return false;
     }
 
